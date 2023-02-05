242a774949d022a3d07a6319bd579dac29f41ffa
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2023-02-05 22:29:04 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,43 @@
 
 int main()
 {
+    using std::string;
+    string channel = "NoCopyrightSounds";
+    string filetype = ".sh";
+    string filename = channel + filetype;
+
+    string indentation = "    ";
+
+    string urlYoutube = "https://www.youtube.com/channel/";
+    string urlChannel = "UC_aEa8K-EOJ3D6gOs7HcyNg";
+    string urlComplete = urlYoutube + urlChannel;
+
+
     using std::ofstream;
-    ofstream outf{"NoCopyrightSounds.sh"};
+    ofstream outf{filename};
 
     if (!outf)
     {
         return 1;
     }
-    else
-    {
-        using std::endl;
-        outf << "#!/bin/bash" << endl;
-        outf << "\n";
-        outf << "\n";
-        outf << "\n";
-        outf << "\n";
-        outf << "clear" << endl;
-        outf << "\n";
-        outf << "\n";
-        outf << "yt-dlp\\" << endl;
-        outf << "    --console-title --no-continue --force-overwrites --no-part --retries 3 --extract-audio --audio-format mp3\\" << endl;
-        outf << "    --output \"../%(channel)s/%(upload_date>%Y-%m-%d)s ─ %(title)s.%(ext)s\"\\" << endl;
-        outf << "    https://www.youtube.com/channel/UC_aEa8K-EOJ3D6gOs7HcyNg";
-        outf << "\n";
-        outf << "\n";
-        outf << "echo -e \"\\n\\n\\n    =========================    PROCESS COMPLETED    =========================    \\n\\n\"" << endl;
-    }
+
+    using std::endl;
+    outf
+    << "#!/bin/bash" << endl
+    << "\n"
+    << "\n"
+    << "\n"
+    << "\n"
+    << "clear" << endl
+    << "\n"
+    << "\n"
+    << "yt-dlp\\" << endl << indentation
+    << "--console-title --no-continue --force-overwrites --no-part --retries 3 --extract-audio --audio-format mp3\\" << endl << indentation
+    << "--output \"../%(channel)s/%(upload_date>%Y-%m-%d)s ─ %(title)s.%(ext)s\"\\" << endl << indentation
+    << urlComplete
+    << "\n"
+    << "\n"
+    << "echo -e \"\\n\\n\\n    =========================    PROCESS COMPLETED    =========================    \\n\\n\"" << endl;
 
     return 0;
 }
