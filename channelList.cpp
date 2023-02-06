8e4feffe26bf957d8031e793431e42fd9af5aed7
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2023-02-06 02:18:47 +0100

    Add files via upload

---
+++ channelList.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+
+
+
+std::vector<std::string> channelList()
+{
+    std::vector<std::string> list {
+        "NoCopyrightSounds",
+        "UC_aEa8K-EOJ3D6gOs7HcyNg",
+    };
+
+
+    return list;
+}
---
+++ flags.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+
+
+
+using std::string;
+
+string flagstring()
+{
+    std::vector<string> flags {
+    "--force-overwrites\\\n    ",
+    "--no-overwrites\\\n    ",
+
+    "--console-title ",
+    "--no-continue ",
+    "--no-part ",
+    "--retries 3 ",
+
+    "--extract-audio --audio-format mp3",
+    "\\"
+    };
+
+
+    string allflags;
+    for (std::size_t i = 0; i < flags.size(); i++)
+    {
+        if (i != 0)
+        {
+            allflags += flags.at(i);
+        }
+    }
+    const string allflagsconst = allflags;
+
+
+    return allflagsconst;
+}
