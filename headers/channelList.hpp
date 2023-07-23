84c5bf694841789b9e07b5e56257847b1d72f87b
Thursday the German <95447983+ThursdayTG@users.noreply.github.com>
2023-07-24 01:00:54 +0200

    Delete headers directory

--- headers/channelList.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-
-
-
-std::vector<std::string> channelList();
--- headers/flags.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-
-
-
-std::string flagstring();
